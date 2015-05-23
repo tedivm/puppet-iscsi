0aa4c7ca3ec1579e40acae48c5dac7c2f4e7d326
Robert Hafner <tedivm@tedivm.com>
2015-05-22 21:00:58 -0700

    Made fact more robust

--- lib/facter/iscsi_initiator.rb
+++ lib/facter/iscsi_initiator.rb
@@ -1,6 +1,9 @@
-
 Facter.add('iscsi_initiator') do
   setcode do
-    Facter::Core::Execution.exec('/usr/bin/tail -1 /etc/iscsi/initiatorname.iscsi | /usr/bin/cut -d "=" -f2')
+    if File.exist? "/etc/iscsi/initiatorname.iscsi"
+      Facter::Core::Execution.exec('/usr/bin/tail -1 /etc/iscsi/initiatorname.iscsi | /usr/bin/cut -d "=" -f2')
+    else
+      false
+    end
   end
 end
